10dc18c027c4a1c087ace1bbeb94725065ea7177
saraluba <81421997+saraluba@users.noreply.github.com>
2021-03-27 16:23:55 +0100

    Add files via upload

---
+++ SumOfNumbers.java
@@ -0,0 +1,9 @@
+public class SumOfNumbers {
+    public static void main(String[] args) {
+        double sum = 0;
+        for(int i=0; i<args.length; i++){
+            sum = sum + Double.parseDouble(args[i]);
+        }
+        System.out.println(sum);
+    }
+}
---
+++ WordSplit.java
@@ -0,0 +1,9 @@
+public class WordSplit {
+    public static void main(String[] args) {
+        char[] array = args[0].toCharArray();
+        for(char c: array){
+            System.out.println(c);
+        }
+    }
+}
+
