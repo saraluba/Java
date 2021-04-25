5ecedd050506511f91e4bb9abc76dc0151eb01ac
saraluba <81421997+saraluba@users.noreply.github.com>
2021-04-25 23:44:16 +0200

    using LinkedHashSet

--- UniqueSymbols.java
+++ UniqueSymbols.java
@@ -1,20 +1,15 @@
 package Zad2004;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class UniqueSymbols {
     public static void main(String[] args) {
         String text = "This is a test text!";
-        List<Character> uniqueSymbols = new ArrayList<>();
+        Set<Character> uniqueSymbols = new LinkedHashSet<>();
         for (int i = 0; i < text.length() ; i++) {
-            if(!uniqueSymbols.contains(text.charAt(i))){
-                uniqueSymbols.add(text.charAt(i));
-            }
+            uniqueSymbols.add(text.charAt(i));
         }
         System.out.println(uniqueSymbols);
     }
-
-
-
+    
 }
