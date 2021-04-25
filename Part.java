9fc2b051aec7680fc8579a5a0c981701d3c69818
saraluba <81421997+saraluba@users.noreply.github.com>
2021-04-25 23:52:26 +0200

    Update and rename Parts.java to Part.java

--- Part.java
+++ Part.java
@@ -1,11 +1,11 @@
 package Zad2004;
 
-public class Parts {
+public class Part {
     protected int partNumber;
     protected String brand;
     protected String name;
 
-    public Parts(int partNumber, String brand, String name){
+    public Part(int partNumber, String brand, String name){
         this.partNumber = partNumber;
         this.brand = brand;
         this.name = name;
